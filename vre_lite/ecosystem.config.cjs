b19d77b12125fa3cba4195d3bda8e06801de777a
gbayarri <genis.bayarri@irbbarcelona.org>
2025-10-15 08:48:29 +0200

    Fixing VRE lite multiple instances issue

--- vre_lite/ecosystem.config.cjs
+++ vre_lite/ecosystem.config.cjs
@@ -3,8 +3,9 @@ module.exports = {
     {
       name: 'mddb-vre',
       port: '3001',
-      exec_mode: 'cluster',
-      instances: 'max',
+      //exec_mode: 'cluster',
+      //instances: 'max',
+      instances: 1,
       env: {
 	      NODE_ENV: 'production'
       },
