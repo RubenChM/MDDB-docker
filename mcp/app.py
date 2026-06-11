e030e93a78380daa51c486fe7ff33ce4b6403817
gbayarri <genis.bayarri@irbbarcelona.org>
2026-06-11 18:17:41 +0200

    Adding REST API MCP

--- mcp/app.py
+++ mcp/app.py
@@ -7,7 +7,12 @@ base_url = f"http://rest:{os.getenv('REST_INNER_PORT', 3000)}/rest/v1/"
 gr.load_openapi(
     openapi_spec="./swagger.json",
     base_url=base_url,
-    paths=["^/projects$"],
+    paths=[
+        "^/projects$",
+        "^/projects/options$",
+        "^/projects/summary$",
+        "^/projects/[^/]+$",
+    ],
     methods=["GET"],
     auth_token=os.getenv("OPENAPI_AUTH_TOKEN")
 ).launch(
