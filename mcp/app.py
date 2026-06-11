b57fc7ef0fcb9530d098e4376db8e373cea5bd5e
gbayarri <genis.bayarri@irbbarcelona.org>
2026-06-11 10:01:17 +0200

    Adding REST API MCP

---
+++ mcp/app.py
@@ -0,0 +1,28 @@
+import os
+import gradio as gr
+
+# Internal service URL for Docker Swarm communication
+base_url = f"http://rest:{os.getenv('REST_INNER_PORT', 3000)}/rest/v1/"
+
+gr.load_openapi(
+    openapi_spec="./swagger.json",
+    base_url=base_url,
+    paths=[
+        ".*projects(?!/.*(?:structure|trajectory|files/)).*",  # projects except structure, trajectory, files/{filename}
+        ".*references.*",
+        ".*pointers.*",
+        ".*topology.*",
+        ".*analyses.*",
+        ".*chains.*",
+        ".*nodes.*",
+        ".*knowledge.*",
+        ".*filenotes.*",
+    ],
+    methods=["GET"],
+    auth_token=os.getenv("OPENAPI_AUTH_TOKEN")
+).launch(
+    server_name="0.0.0.0",
+    server_port=int(os.getenv("MCP_PORT", 8000)),
+    root_path=f'/{os.getenv("MCP_ROOT_PATH", "mcp")}',
+    mcp_server=True
+)
